9f24f822977c2198674018cb910dcb40a1e56a82
upendarsankoju7 <upendarsankoju7@gmail.com>
2026-06-06 14:40:00 +0530

    Added New Variables File

---
+++ variables.js
@@ -0,0 +1,11 @@
+"use strict";
+// both type and initial value
+var employeeName = "Upendar Sankoju";
+console.log(employeeName);
+var employeeAge = 30;
+console.log(employeeAge);
+var upendar = "Welcome to TypeScript programming";
+console.log(upendar);
+var rajuName;
+rajuName = "Hello Good Morning";
+console.log(rajuName); // undefined
---
+++ variables.ts
@@ -0,0 +1,14 @@
+// both type and initial value
+var employeeName: string = "Upendar Sankoju";
+console.log(employeeName);
+
+var employeeAge: number = 30;
+console.log(employeeAge);
+
+
+var upendar: string ="Welcome to TypeScript programming";
+console.log(upendar);
+
+var rajuName: string;
+rajuName ="Hello Good Morning";
+console.log(rajuName); // undefined
