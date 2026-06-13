f790552df3c17902babf76bb29b1ab7367f09838
upendarsankoju7 <upendarsankoju7@gmail.com>
2026-06-13 08:07:48 +0530

    Added Switch Cases

--- ConditionalStatements.js
+++ ConditionalStatements.js
@@ -1,104 +1,65 @@
-// ====================================================
+"use strict";
 // Conditional Statements in TypeScript
-// Types: if, if-else, else-if, switch, ternary operator
-// ====================================================
-
-// Variable declarations
-var Person_Age: number = 15;
-var num: number = 15;
-var num1: number = 60;
-var num2: number = 50;
-
+// Types: if, if-else, else-if
+var Person_Age = 15;
+var num = 15;
+var num1 = 60;
+var num2 = 50;
 // ====================================================
 // Problem 1: Check whether a person is eligible to vote
 // Condition: Age should be 18 or above
 // ====================================================
-
 if (Person_Age >= 18) {
     console.log("Eligible for Vote");
-} else {
+}
+else {
     console.log("Not Eligible for Vote");
 }
-
-
 // ====================================================
 // Problem 2: Check whether a number is Even or Odd
-// Condition: If the remainder is 0 when divided by 2,
-// then the number is Even; otherwise, it is Odd.
+// Condition: If remainder is 0 when divided by 2,
+// then it is an Even Number.
 // ====================================================
-
 if (num % 2 == 0) {
     console.log("It is an Even Number");
-} else {
+}
+else {
     console.log("It is an Odd Number");
 }
-
-
 // ====================================================
 // Problem 3: Check whether another number is Even or Odd
-// num1 = 60
-// Since 60 is divisible by 2, it is an Even Number.
+// Here num1 = 900
+// Since 900 is divisible by 2, it is an Even Number.
 // ====================================================
-
 if (num1 % 2 == 0) {
     console.log("It is an Even Number");
-} else {
+}
+else {
     console.log("It is an Odd Number");
 }
-
-
-// ====================================================
-// Problem 4: Find the greater number between num1 and num2
-// ====================================================
-
 if (num1 > num2) {
     console.log("High Number");
-} else {
+}
+else {
     console.log("Less Number");
 }
-
-
-// ====================================================
-// Problem 5: Find the largest number among three numbers
-// Using else-if condition
-// ====================================================
-
-var a: number = 50;
-var b: number = 20;
-var c: number = 30;
-
+var a = 50;
+var b = 20;
+var c = 30;
 if (a > b && a > c) {
     console.log("A Largest Number");
 }
-else if (b > a && b > c) {
+else if (b > c && b > a) {
     console.log("B Largest Number");
 }
 else {
     console.log("C Largest Number");
 }
-
-
-// ====================================================
-// Problem 6: Ternary Operator
-// Short form of if-else statement
-// Syntax:
-// condition ? true_statement : false_statement
-// ====================================================
-
-var x: number = 200;
-var y: number = 300;
-
-x > y
-    ? console.log("X is The Larger Number")
-    : console.log("Y is The Larger Number");
-
-
-// ====================================================
-// Problem 7: Display Day Name using else-if ladder
-// ====================================================
-
-var weekNumber: number = 1;
-
+//trimaninator operator 
+var x = 200;
+var y = 300;
+x > y ? console.log("X is The Larger Number") : console.log("Y is The Larger Number");
+var weekNumber = 1;
 if (weekNumber == 1) {
     console.log("Sunday");
 }
@@ -121,47 +82,34 @@ else if (weekNumber == 7) {
     console.log("Saturday");
 }
 else {
-    console.log("Invalid Day");
+    console.log("Invaild day");
 }
-
-
-// ====================================================
-// Problem 8: Display Day Name using Switch Case
-// Switch is useful when checking a single variable
-// against multiple possible values.
-// ====================================================
-
+// Variable to store the week number
+var weekNumber = 1;
+// Switch statement to check the day
 switch (weekNumber) {
-
     case 1:
         console.log("Sunday");
         break;
-
     case 2:
         console.log("Monday");
         break;
-
     case 3:
         console.log("Tuesday");
         break;
-
     case 4:
         console.log("Wednesday");
         break;
-
     case 5:
         console.log("Thursday");
         break;
-
     case 6:
         console.log("Friday");
         break;
-
     case 7:
         console.log("Saturday");
         break;
-
     default:
-        console.log("Invalid Day");
+        console.log("Invalid day");
         break;
 }
